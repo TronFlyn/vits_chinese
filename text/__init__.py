96aa6ff90e28526c776d7a38d5a0a727fa4dd844
TronFlyn <35795553+TronFlyn@users.noreply.github.com>
2022-08-28 09:03:23 +0800

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -1,6 +1,6 @@
 """ from https://github.com/keithito/tacotron """
 from text import cleaners
-from text.symbols import symbols,symbols_zh
+from text.symbols import symbols
 
 
 # Mappings from symbol to numeric ID and vice versa:
